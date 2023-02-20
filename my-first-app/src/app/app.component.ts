fdf79cf6160b8a12a93129028719fb10ee716ffa
Parastou Safajou <pr2.leclair@gmail.com>
2023-02-19 23:07:33 -0800

    Revert "New Components"
    This reverts commit 27f88ee4580e70f9aaa96f38a9a993d6ecb2dff3.

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -3,14 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.scss']
-  styles: [
-    `
-      p {
-        color: red;
-      }
-    `,
-  ],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'my-first-app';
